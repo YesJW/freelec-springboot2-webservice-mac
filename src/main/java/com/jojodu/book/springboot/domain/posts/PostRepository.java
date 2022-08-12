42fedfd29fd2e978893beb20e2f774e95ae4a792
Jangwoo No <ssk01017@gmail.com>
2022-08-13 01:04:47 +0900

    fixed untracked files

--- src/main/java/com/jojodu/book/springboot/domain/posts/PostRepository.java
+++ src/main/java/com/jojodu/book/springboot/domain/posts/PostRepository.java
@@ -1,7 +1,11 @@
 package com.jojodu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Posts, Long> { //JpaRepository<Entiry 클래스, PK 타입>
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -2,19 +2,25 @@ package com.jojodu.book.springboot.service.posts;
 
 import com.jojodu.book.springboot.domain.posts.PostRepository;
 import com.jojodu.book.springboot.domain.posts.Posts;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import  org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostRepository repository;
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return repository.save(requestDto.toEntity()).getId();
     }
@@ -33,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return repository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = repository.findById(id)
+                .orElseThrow(() -> new IllegalIdentifierException("해당 게시글이 없습니다. id= "+ id));
+        repository.delete(posts);
+
     }
 }
